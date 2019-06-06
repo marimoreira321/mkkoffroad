5609db16777450a19fe9eae269f0ec192e2a73de
Andre <mury_gh@hotmail.com>
2019-06-06 00:20:04 -0300

    homepage slider and product catalog style ok]

--- wp-content/themes/storefront/home-page.php
+++ wp-content/themes/storefront/home-page.php
@@ -1,31 +1,10 @@
-<h1>Home</h1>
-<div class="row mb-5 py-3">
+<div class="row mb-5">
     <div class="col-12" id="home-page">
-        <div id="home-carousel" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#home-carousel" data-slide-to="0" class="active"></li>
-                <li data-target="#home-carousel" data-slide-to="1"></li>
-                <li data-target="#home-carousel" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="https://images.tcdn.com.br/img/img_prod/633652/1530578200_img3.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="https://images.tcdn.com.br/img/img_prod/633652/1530578201_img4.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="https://images.tcdn.com.br/img/img_prod/633652/1530578198_img2.jpg" class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#home-carousel" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#home-carousel" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
+        <?php
+            echo do_shortcode('[smartslider3 slider=2]');
+        ?>
     </div>
+</div>
+<div class="row" id="portfolio">
+
 </div>
