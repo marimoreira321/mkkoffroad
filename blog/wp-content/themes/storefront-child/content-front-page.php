b374f644f17fac396bbd31a14c181fc512d23eef
André Mury <mury_gh@hotmail.com>
2019-08-25 23:05:25 -0300

    removing old info

--- blog/wp-content/themes/storefront-child/content-front-page.php
+++ blog/wp-content/themes/storefront-child/content-front-page.php
@@ -19,15 +19,18 @@ $t_uri = get_template_directory_uri().'-child';
 		<div class="container w-100 bg-white py-5">
 			<div class="row justify-content-center">
 				<div class="col-12 col-sm-3">
-					<product title="BAGS UNIVERSAIS" :thumb="'<?= $t_uri.'/assets/images/diminas/cat_01.webp' ?>'" :uri="'/'">
+					<product title="BAGS UNIVERSAIS" :thumb="'<?= $t_uri.'/assets/images/diminas/cat_01.webp' ?>'"
+						:uri="'/'">
 					</product>
 				</div>
 				<div class="col-12 col-sm-3">
-					<product title="ILUMINAÇÃO" :thumb="'<?= $t_uri.'/assets/images/diminas/cat_02.webp' ?>'" :uri="'/'">
+					<product title="ILUMINAÇÃO" :thumb="'<?= $t_uri.'/assets/images/diminas/cat_02.webp' ?>'"
+						:uri="'/'">
 					</product>
 				</div>
 				<div class="col-12 col-sm-3">
-					<product title="ACESSÓRIOS" :thumb="'<?= $t_uri.'/assets/images/diminas/cat_03.webp' ?>'" :uri="'/'">
+					<product title="ACESSÓRIOS" :thumb="'<?= $t_uri.'/assets/images/diminas/cat_03.webp' ?>'"
+						:uri="'/'">
 					</product>
 				</div>
 				<div class="col-12 col-sm-3">
@@ -53,8 +56,7 @@ $t_uri = get_template_directory_uri().'-child';
 						:uri="'/'"></product>
 				</div>
 				<div class="col-12 col-sm-3">
-					<product title="TECNOLOGIA" :thumb="'<?= $t_uri.'/assets/images/diminas/it_04.webp' ?>'"
-						:uri="'/'">
+					<product title="TECNOLOGIA" :thumb="'<?= $t_uri.'/assets/images/diminas/it_04.webp' ?>'" :uri="'/'">
 					</product>
 				</div>
 			</div>
@@ -62,45 +64,9 @@ $t_uri = get_template_directory_uri().'-child';
 	</div>
 </div>
 
-<div id="contact" class="row">
-	<div class="col-12 col-sm-6">
-		<h3 class="text-center t-brand-red text-strong mt-5">Siga-nos</h3>
-	</div>
-	<div class="col-12 col-sm-6">
-		<h3 class="text-center t-brand-red text-strong mt-5">Contato</h3>
-		<form class="d-flex mt-5 justify-content-center">
-			<div class="form-group t-dark f-concert-one d-flex flex-column vw-30 text-left">
 
-				<label>Seu nome: <br />
-					<input class="form-control shadow-none my-1" v-model="name" type="tel">
-				</label>
-				<label>Telefone: <br />
-					<input class="form-control shadow-none my-1" v-model="phone" type="tel">
-				</label>
-				<label>E-mail: <br />
-					<input class="form-control shadow-none my-1" v-model="email" type="email">
-				</label>
-				<label>Sua mensagem: <br />
-					<textarea class="form-control my-1" v-model="content"></textarea>
-				</label><br />
-				<button class="btn btn-success" @click="sendMail" type="button">Enviar!</button>
-			</div>
-		</form>
-		<br /><small class="t-gray">Você também pode enviar um e-mail para:<br />
-			<a href="mailto:contato@laticiniosdiminas.com.br">onpharma@outlook.com</a>
-		</small>
-	</div>
-</div>
 
-<div id="notes" class="row">
-	<div class="col-12 col-sm-6">
-		<h4>Indústria - Rua Aquiles, </h4>
-	</div>
-	<div class="col-12 col-sm-6">
 
-	</div>
-</div>
-</div>
 
 
 <script defer src="<?= $t_uri.'/assets/pjs/front-page.min.js' ?>"></script>
