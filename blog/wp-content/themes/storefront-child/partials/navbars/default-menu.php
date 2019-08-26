1967a7558f7227245ed908f55b1028d6911728fd
André Mury <mury_gh@hotmail.com>
2019-08-25 23:33:28 -0300

    adding dbconf local

--- blog/wp-content/themes/storefront-child/partials/navbars/default-menu.php
+++ blog/wp-content/themes/storefront-child/partials/navbars/default-menu.php
@@ -27,12 +27,12 @@
     <div class="row mb-3">
         <div id="menu_part" class="col-12 d-flex align-items-center">
         <img class="d-md-none" src="<?php echo get_template_directory_uri().'/assets/images/diminas/default.png'?>" style="height: 100px">
-            <ul id="menu_right" class="d-none m-0 d-md-flex pt-5 py-sm-0 px-0 px-sm-3 align-items-center text-strong w-100 justify-content-center">
-                <li class="menu-item ml-md-5 p-3"><a href="<?= get_site_url() ?>">HOME</a></li>
-                <li class="menu-item p-3"><a href="<?= get_site_url() ?>/loja">PRODUTOS</a></li>
-                <li class="menu-item p-3"><a href="<?= get_site_url() ?>/">SOBRE NÓS</a></li>
-                <li class="menu-item p-3"><a href="<?= get_site_url() ?>/">FAQ</a></li>
-                <li class="menu-item p-3"><a href="<?= get_site_url() ?>/">CONTATO</a></li>
+            <ul id="menu_right" class="d-none m-0 d-md-flex pt-5 py-sm-0 px-0 px-sm-3 align-items-center  w-100 justify-content-center">
+                <li class="menu-item p-3 font-12px"><a href="<?= get_site_url() ?>">HOME</a></li>
+                <li class="menu-item p-3 font-12px"><a href="<?= get_site_url() ?>/loja">PRODUTOS</a></li>
+                <li class="menu-item p-3 font-12px"><a href="<?= get_site_url() ?>/">SOBRE NÓS</a></li>
+                <li class="menu-item p-3 font-12px"><a href="<?= get_site_url() ?>/">FAQ</a></li>
+                <li class="menu-item p-3 font-12px"><a href="<?= get_site_url() ?>/">CONTATO</a></li>
                 <!-- <li class="menu-item p-3"><a href="/">ONDE COMPRAR</a></li> -->
             </ul>
         </div>
